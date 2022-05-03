ab18b1bba4662d65c823aea129e287ceebeceeb6
PetrFlajsingr <flajsingr@gmail.com>
2022-05-03 19:28:51 +0200

    Added `ComputeShaderProgram` class

---
+++ src/gpu/ComputeShaderProgram.cpp
@@ -0,0 +1,7 @@
+//
+// Created by xflajs00 on 03.05.2022.
+//
+
+#include "ComputeShaderProgram.h"
+
+namespace pf {}
---
+++ src/gpu/ComputeShaderProgram.h
@@ -0,0 +1,13 @@
+//
+// Created by xflajs00 on 03.05.2022.
+//
+
+#ifndef PF_RENDERING_PLAYGROUND_COMPUTESHADERPROGRAM_H
+#define PF_RENDERING_PLAYGROUND_COMPUTESHADERPROGRAM_H
+
+namespace pf {
+
+class ComputeShaderProgram {};
+
+}  // namespace pf
+#endif  //PF_RENDERING_PLAYGROUND_COMPUTESHADERPROGRAM_H
