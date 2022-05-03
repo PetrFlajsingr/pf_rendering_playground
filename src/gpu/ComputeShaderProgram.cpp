c5c8af4634a919567d7308336dd8bd3d51baf626
PetrFlajsingr <flajsingr@gmail.com>
2022-05-03 20:26:13 +0200

    Added shader creation to `ComputeShaderProgram`

--- src/gpu/ComputeShaderProgram.cpp
+++ src/gpu/ComputeShaderProgram.cpp
@@ -4,4 +4,65 @@
 
 #include "ComputeShaderProgram.h"
 
-namespace pf {}
+namespace pf {
+
+ComputeShaderProgram::~ComputeShaderProgram() { glDeleteProgram(programHandle); }
+
+std::optional<GLuint> ComputeShaderProgram::CreateShaderHandle(std::span<unsigned int> spirvData) {
+  const auto shaderHandle = glCreateShader(GL_COMPUTE_SHADER);
+
+  glShaderBinary(1, &shaderHandle, GL_SHADER_BINARY_FORMAT_SPIR_V, spirvData.data(),
+                 spirvData.size() * sizeof(decltype(spirvData)::value_type));
+  glSpecializeShader(shaderHandle, "main", 0, nullptr, nullptr);
+
+  GLint compiled{};
+  glGetShaderiv(shaderHandle, GL_COMPILE_STATUS, &compiled);
+  if (compiled == GL_FALSE) {
+    int errMessageLength{};
+    std::string errorMessage;
+    glGetShaderiv(shaderHandle, GL_INFO_LOG_LENGTH, &errMessageLength);
+    if (errMessageLength != 0) {
+      errorMessage.resize(errMessageLength);
+      glGetShaderInfoLog(shaderHandle, errMessageLength, nullptr, errorMessage.data());
+    }
+    spdlog::error("Shader compilation failed");
+    spdlog::error("Message: {}", errorMessage);
+    glDeleteShader(shaderHandle);
+    return std::nullopt;
+  }
+  return shaderHandle;
+}
+
+std::optional<GLuint> ComputeShaderProgram::CreateProgramHandle(GLuint shaderHandle) {
+  const auto programHandle = glCreateProgram();
+  glAttachShader(programHandle, shaderHandle);
+
+  glLinkProgram(programHandle);
+
+  GLint linkSuccess;
+  glGetProgramiv(programHandle, GL_LINK_STATUS, &linkSuccess);
+  if (linkSuccess == GL_FALSE) {
+    int errMessageLength{};
+    std::string errorMessage;
+    glGetProgramiv(programHandle, GL_INFO_LOG_LENGTH, &errMessageLength);
+    if (errMessageLength != 0) {
+      errorMessage.resize(errMessageLength);
+      glGetProgramInfoLog(programHandle, errMessageLength, nullptr, errorMessage.data());
+    }
+    spdlog::error("Program linking failed");
+    spdlog::error("Message: {}", errorMessage);
+    glDeleteProgram(programHandle);
+    return std::nullopt;
+  }
+  return programHandle;
+}
+
+void ComputeShaderProgram::findUniformLocations() {
+  // TODO: infer type on its own
+  std::ranges::for_each(uniforms, [this](Uniform &uniform) {
+    const auto uniformLocation = glGetUniformLocation(programHandle, uniform.name.c_str());
+    if (uniformLocation != -1) { uniform.location = uniformLocation; }
+  });
+}
+
+}  // namespace pf
--- src/gpu/ComputeShaderProgram.h
+++ src/gpu/ComputeShaderProgram.h
@@ -5,9 +5,65 @@
 #ifndef PF_RENDERING_PLAYGROUND_COMPUTESHADERPROGRAM_H
 #define PF_RENDERING_PLAYGROUND_COMPUTESHADERPROGRAM_H
 
+#include "spdlog/spdlog.h"
+#include <glad/glad.h>
+#include <pf_common/concepts/ranges.h>
+#include <span>
+#include <tl/expected.hpp>
+#include <unordered_map>
+#include <utils/glsl_typenames.h>
+
 namespace pf {
 
-class ComputeShaderProgram {};
+class ComputeShaderProgram {
+ public:
+  using Error = std::string;
+  class Uniform {
+    friend class ComputeShaderProgram;
+
+   public:
+    template<OneOf<PF_GLSL_TYPES> T>
+    explicit Uniform(std::string uniformName) : name(std::move(uniformName)), typeName(getGLSLTypeName<T>()) {}
+
+   private:
+    std::string name;
+    std::string typeName;
+    std::optional<GLint> location = std::nullopt;
+  };
+
+  [[nodiscard]] static tl::expected<ComputeShaderProgram, Error> Create(std::span<unsigned int> spirvData,
+                                                                        RangeOf<Uniform> auto &&shaderUniforms);
+
+  ComputeShaderProgram(GLuint programHandle, RangeOf<Uniform> auto &&shaderUniforms)
+      : programHandle(programHandle), uniforms{std::ranges::begin(shaderUniforms), std::ranges::end(shaderUniforms)} {
+    findUniformLocations();
+  }
+  ~ComputeShaderProgram();
+
+ private:
+  [[nodiscard]] static std::optional<GLuint> CreateShaderHandle(std::span<unsigned int> spirvData);
+  [[nodiscard]] static std::optional<GLuint> CreateProgramHandle(GLuint shaderHandle);
+
+  void findUniformLocations();
+
+  GLuint programHandle;
+  std::vector<Uniform> uniforms;
+};
+
+tl::expected<ComputeShaderProgram, ComputeShaderProgram::Error>
+ComputeShaderProgram::Create(std::span<unsigned int> spirvData, RangeOf<Uniform> auto &&shaderUniforms) {
+  const auto shaderHandle = CreateShaderHandle(spirvData);
+  if (!shaderHandle.has_value()) { return tl::make_unexpected("Shader creation failed"); }
+
+  const auto programHandle = CreateProgramHandle(shaderHandle.value());
+  // either we need to delete shader because program is not valid or mark it for deletion when program gets deleted
+  glDeleteShader(shaderHandle.value());
+  if (!programHandle.has_value()) { return tl::make_unexpected("Program creation failed"); }
+
+  return ComputeShaderProgram{
+      programHandle.value(),
+      std::vector<Uniform>{std::ranges::begin(shaderUniforms), std::ranges::end(shaderUniforms)}};
+}
 
 }  // namespace pf
 #endif  //PF_RENDERING_PLAYGROUND_COMPUTESHADERPROGRAM_H
